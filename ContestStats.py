340a545c0758d684a160e7f227f95e07fd40eda1
Jens Abraham <jensabr@gmail.com>
2013-04-11 21:25:38 +0200

    [ContestStats.py] added break-time calculation and added handling of 4 and 6 signs 'time_on' field

--- ContestStats.py
+++ ContestStats.py
@@ -1,6 +1,6 @@
 __author__ = 'dk2ab'
 
-import time
+from datetime import datetime
 import re
 
 class ContestStats:
@@ -25,7 +25,7 @@ class ContestStats:
     def ratesPerHour(self):
         ratesPerHour = {}
         for qso in self.log:
-            t = time.strptime(qso['qso_date']+qso['time_on'][0:2], '%Y%m%d%H')
+            t = datetime.strptime(qso['qso_date']+qso['time_on'][0:2], '%Y%m%d%H')
             if t not in ratesPerHour:
                 ratesPerHour[t] = {}
             self.byBand(ratesPerHour[t], qso)
@@ -58,7 +58,27 @@ class ContestStats:
     def prefixes(self):
         prefixes = set()
         for qso in self.log:
-            prefix = re.findall('([\w]{1,2}[\d]+)', qso['call'])
+            prefix = re.findall('(^[\w]{1,2}[\d]+)', qso['call'])
             assert len(prefix) == 1, "There might be an error in the prefix-regexp (%s, %s)" % (qso['call'], prefix)
             prefixes.add(prefix[0])
         return sorted(prefixes)
+
+    def breakTimes(self, contestStart, contestEnd, minBreak):
+        offTimes = []
+        tlast = contestStart
+        for qso in self.log:
+            if len(qso['time_on']) == 4:
+                t = datetime.strptime(qso['qso_date']+qso['time_on'], '%Y%m%d%H%M')
+            elif len(qso['time_on']) == 6:
+                t = datetime.strptime(qso['qso_date']+qso['time_on'], '%Y%m%d%H%M%S')
+
+            tdiff = t - tlast
+            if tdiff > minBreak:
+                offTimes.append({'start': tlast, 'end': t, 'diff': t - tlast})
+            tlast = t
+
+        tdiff = contestEnd - tlast
+        if tdiff > minBreak:
+            offTimes.append({'start': tlast, 'end': contestEnd, 'diff': contestEnd - tlast})
+
+        return offTimes
