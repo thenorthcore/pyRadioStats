a54422841225ab3db5e9a5211caf7f4e955f2cfc
Jens Abraham <jensabr@gmail.com>
2013-04-11 21:26:12 +0200

    [stats.py] show simple on/off times statistic

--- stats.py
+++ stats.py
@@ -3,12 +3,12 @@
 
 __author__ = 'dk2ab'
 
-import time
+from datetime import datetime, timedelta
 from Parser import Parser
 from ContestStats import ContestStats
  
 file_name = '121125-cqwwcw.ADI'
-file_name = '2009-September.adi'
+#file_name = '2009-September.adi'
 
 l = Parser.parseADIF(file_name)
 s = ContestStats(l)
@@ -18,4 +18,18 @@ print(s.qsosByBand())
 print(s.qsosByOperator())
 rph = s.ratesPerHour()
 for hour in sorted(rph.keys()):
-    print('%s -> %s' % (time.asctime(hour), rph[hour]))
+    print('%s -> %s' % (hour, rph[hour]))
+print(s.qsosByMode())
+print(s.prefixes())
+conteststart = datetime.strptime('201211240000', '%Y%m%d%H%M')
+contestend = datetime.strptime('201211252359', '%Y%m%d%H%M')
+
+bt = s.breakTimes(conteststart, contestend, timedelta(minutes=60))
+sumoff = timedelta(0)
+print(bt)
+for b in bt:
+    print('%s -> %s = %s' % (b['start'], b['end'], b['diff']))
+    sumoff += b['diff']
+
+print('Sum offtime: %s' % sumoff)
+print('Sum ontime: %s' % ((contestend-conteststart)-sumoff))
